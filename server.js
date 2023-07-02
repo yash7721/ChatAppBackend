77b3f75febcc164b42973de9a809a714343346db
yash7721 <77916043+yash7721@users.noreply.github.com>
2023-07-03 01:43:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
-const cors=require(cors);
+const cors=require("cors");
 dotenv.config();
 connectDB();
 const app = express();
