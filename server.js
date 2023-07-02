8805cf2dd83685f0b486e2718d918cb259af246f
yash7721 <77916043+yash7721@users.noreply.github.com>
2023-07-03 01:38:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,17 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
-// const cors=require(cors);
+const cors=require(cors);
 dotenv.config();
 connectDB();
 const app = express();
 
 app.use(express.json()); // to accept json data
-// app.use(cors());
-// app.get("/", (req, res) => {
-//   res.send("API Running!");
-// });
+app.use(cors());
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials","true);
+  res.send("API Running!");
+});
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
